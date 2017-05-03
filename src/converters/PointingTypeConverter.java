26cb1df041769645febffd0c9f77d69c1e85f365
vivek <vivekvenkris@gmail.com>
2017-05-03 20:11:25 +1000

    switch to else if ladder because either Java is stupid or I am.

--- src/converters/PointingTypeConverter.java
+++ src/converters/PointingTypeConverter.java
@@ -11,7 +11,7 @@ public class PointingTypeConverter implements Converter {
 
 	@Override
 	public Object getAsObject(FacesContext arg0, UIComponent arg1, String arg2) {
-		
+
 		switch(arg2){
 		case  "Candidate":
 			return SMIRFConstants.candidatePointingSymbol;
@@ -36,27 +36,28 @@ public class PointingTypeConverter implements Converter {
 	}
 
 	@Override
-	public String getAsString(FacesContext arg0, UIComponent arg1, Object arg2) {
-		switch((String)arg2){
-		case SMIRFConstants.candidatePointingSymbol:
-			return "Candidate";
-		case SMIRFConstants.smcPointingSymbol:
-			return "SMC";
-		case SMIRFConstants.lmcPointingSymbol:
-			return "LMC";
-		case SMIRFConstants.galacticPointingSymbol:
-			return "Galactic";
-		case SMIRFConstants.phaseCalibratorSymbol:
-			return "PhaseCal";
-		case SMIRFConstants.fluxCalibratorSymbol:
-			return "FluxCal";
-		case SMIRFConstants.randomPointingSymbol:
-			return "Random";
-		case "All":
-			return "All";	
-		default:
-			return "Unknown";
+	public String getAsString(FacesContext arg0, UIComponent arg1, Object obj) {
 
+
+		String arg2 = (String)obj;
+		if (SMIRFConstants.candidatePointingSymbol.equals(arg2)) {
+			return "Candidate";
+		} else if (SMIRFConstants.smcPointingSymbol.equals(arg2)) {
+			return "SMC";
+		} else if (SMIRFConstants.lmcPointingSymbol.equals(arg2)) {
+			return "LMC";
+		} else if (SMIRFConstants.galacticPointingSymbol.equals(arg2)) {
+			return "Galactic";
+		} else if (SMIRFConstants.phaseCalibratorSymbol.equals(arg2)) {
+			return "PhaseCal";
+		} else if (SMIRFConstants.fluxCalibratorSymbol.equals(arg2)) {
+			return "FluxCal";
+		} else if (SMIRFConstants.randomPointingSymbol.equals(arg2)) {
+			return "Random";
+		} else if (arg2.equals("All")) {
+			return "All";
+		} else {
+			return "Unknown";
 		}	
 	}
 
