8abf5862ad2d6b54d9b010d3df0661613641fef1
vivek <vivekvenkris@gmail.com>
2017-05-03 20:03:49 +1000

    Formatting

--- src/service/CalibrationService.java
+++ src/service/CalibrationService.java
@@ -8,16 +8,20 @@ import util.Utilities;
 public class CalibrationService {
 	
 	public boolean Calibrate(String utc, String calibratorName) throws IOException, InterruptedException{
+		
 			System.err.println("Starting calibration for UTC:" + utc);
 			String process = "cd "+BackendConstants.resultsDir+"/"+utc+ "/"+calibratorName+ "; "+ BackendConstants.calibrateScript;
+			
 			System.err.println("running: "+ process);
 			String response =Utilities.runSSHProcess(BackendConstants.dadaAtLocalhost, process, null, true);
+			
 			//System.err.println(response);
 			process = "cd "+BackendConstants.resultsDir+"/"+utc+"/"+calibratorName+ "; "+BackendConstants.updateDelays;
 			System.err.println("running: "+ process);
 			response = Utilities.runSSHProcess(BackendConstants.dadaAtLocalhost, process, null, true);
 			System.err.println(response);
-			System.err.println("all done.");
+			
+			System.err.println("calibrated.");
 			return true;
 			
 
