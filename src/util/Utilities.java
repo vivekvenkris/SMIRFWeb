7f0e055d4ec903f065f2b475022d2fc5a3e6d27f
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2017-05-07 01:54:27 +1000

    added createDirectoryStructure(varargs) function

--- src/util/Utilities.java
+++ src/util/Utilities.java
@@ -232,4 +232,13 @@ public class Utilities {
 		
 	}
 	
+	public static String createDirectoryStructure(String...names){
+		String result = "";
+		for( int i=0; i< names.length; i++){
+			result += names[i];
+			if(i != names.length -1 ) result += "/";
+		}
+		return result;
+	}
+	
 }
