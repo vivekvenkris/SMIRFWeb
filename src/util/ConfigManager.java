9be0f11f757d1ab2eb61fe20ce0bd5c8bbbe1b8a
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2017-05-15 04:34:34 +1000

    Changes to use BS instead of node names

--- src/util/ConfigManager.java
+++ src/util/ConfigManager.java
@@ -1,6 +1,7 @@
 package util;
 
 import java.io.BufferedReader;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -14,11 +15,16 @@ import java.util.Map.Entry;
 
 import org.javatuples.Pair;
 
+import exceptions.InvalidFanBeamNumberException;
+
 
 public class ConfigManager {
 	
 	public static void main(String[] args) throws IOException {
 		loadConfigs();
+		
+		System.err.println(nodeBsBpFbMap);
+		
 	}
 	
 	private static  Map<String, Map<Integer, Integer>>  nepenthesServers;
@@ -29,12 +35,15 @@ public class ConfigManager {
 	private static Map<String, String> mopsrBpCornerturnMap;
 	private static Map<String, String> mopsrBsMap;
 	
+	//node, bs, start FB, end FB
+	private static Map<String, Map<Integer, Pair<Integer, Integer > > > beamBoundariesMap = new HashMap<>();
+	// node bs bp start FB end FB
+	private static Map<String, Map<Integer, Map<Integer, Pair<Integer, Integer > > > > nodeBsBpFbMap = new HashMap<>();
 	
-	private static Map<String, Pair<Integer, Integer > > beamBoundariesMap = new HashMap<>();
 	
 	private static Integer numFanBeams;
 	private static String  edgeNode;
-	private static String  edgeBS;
+	private static Integer  edgeBS;
 
 
 	private static Map<String, List<Integer>> activeBSForNodes = new HashMap<>();
@@ -52,14 +61,13 @@ public class ConfigManager {
 		
 		smirfMap = readConfig(SMIRFConstants.smirfConfig);
 		smirfConventionsMap = readConfig(SMIRFConstants.smirfConventionsConfig);
-		
 		mopsrMap = readConfig( Utilities.createDirectoryStructure(smirfMap.get("CONFIG_ROOT"),smirfMap.get("MOPSR_CONFIG")));
 		mopsrBpMap = readConfig(Utilities.createDirectoryStructure(smirfMap.get("CONFIG_ROOT"),smirfMap.get("MOPSR_BP_CONFIG")));
 		mopsrBpCornerturnMap = readConfig(Utilities.createDirectoryStructure(smirfMap.get("CONFIG_ROOT"),smirfMap.get("MOPSR_BP_CORNERTURN_CONFIG")));
 		mopsrBsMap = readConfig(Utilities.createDirectoryStructure(smirfMap.get("CONFIG_ROOT"),smirfMap.get("MOPSR_BS_CONFIG")));
 				
 		edgeNode = smirfMap.get("EDGE_NODE");
-		edgeBS = smirfMap.get("EDGE_BS");
+		edgeBS = Integer.parseInt(smirfMap.get("EDGE_BS"));
 		
 		
 		
@@ -96,34 +104,90 @@ public class ConfigManager {
 		
 		Integer numBP = Integer.parseInt(mopsrBpMap.get("NUM_BP"));
 		
-		for(String node: activeBSForNodes.keySet()){
+		for(Entry<String, List<Integer>> entry: activeBSForNodes.entrySet()){
+			String node = entry.getKey();
+			for(Integer bs: entry.getValue()){
+				
+				List<Integer> beamProcessors = new ArrayList<>();
+				
+				if(node.equals(edgeNode)) 	{
+					
+					Map<Integer, Pair<Integer, Integer>> map = beamBoundariesMap.getOrDefault(node, new HashMap<>());
+					map.put(bs, new Pair<Integer, Integer>( 1 , numFanBeams));
+					beamBoundariesMap.put(node, map);	
+					
+					Map<Integer, Map<Integer, Pair<Integer, Integer>>> bsBpFbMap = nodeBsBpFbMap.getOrDefault(node, new HashMap<>() );
+					Map<Integer, Pair<Integer, Integer>> bpFbMap = bsBpFbMap.getOrDefault(bs, new HashMap<>());
+					for(int bp=0; bp< numBP; bp++)	bpFbMap.put(bp, new Pair<Integer, Integer>( 1 , numFanBeams));
+					bsBpFbMap.put(bs, bpFbMap);
+					nodeBsBpFbMap.put(node, bsBpFbMap);
+
+					
+					continue;
+					
+				}
+				
+				for( int bp=0 ; bp< numBP; bp++ ) if( mopsrBpMap.get("BP_" + bp).equals(node) ) beamProcessors.add(bp);
+				Integer minFB = null, maxFB = null;
+				
+				Map<Integer, Map<Integer, Pair<Integer, Integer>>> bsBpFbMap = nodeBsBpFbMap.getOrDefault(node, new HashMap<>() );
+				Map<Integer, Pair<Integer, Integer>> bpFbMap = bsBpFbMap.getOrDefault(bs, new HashMap<>());
+				
+				for(Integer bp: beamProcessors) {
+					
+					Integer min = Integer.parseInt(mopsrBpCornerturnMap.get("BEAM_FIRST_RECV_" + bp));
+					Integer max = Integer.parseInt(mopsrBpCornerturnMap.get("BEAM_LAST_RECV_" + bp));
+					
+					
+					if(minFB == null || minFB > min)  minFB = min;
+					if(maxFB == null || maxFB < max)  maxFB = max;
+					
+					bpFbMap.put(bp, new Pair<Integer, Integer>( min+1, max + 1));
+				}
+				Map<Integer, Pair<Integer, Integer>> map = beamBoundariesMap.getOrDefault(node, new HashMap<>());
+				map.put(bs, new Pair<Integer, Integer>( minFB + 1 ,  maxFB + 1));
+				beamBoundariesMap.put(node, map);	
+				
 			
-			if(node.equals(edgeNode)) 	{
-				beamBoundariesMap.put(node, new Pair<Integer, Integer>( 1 , numFanBeams));
-				continue;
+				bsBpFbMap.put(bs, bpFbMap);
+				nodeBsBpFbMap.put(node, bsBpFbMap);
+
 			}
-			
-			List<Integer> beamProcessors = new ArrayList<>();
-			
-			for( int bp=0 ; bp< numBP; bp++ ) if( mopsrBpMap.get("BP_" + bp).equals(node) ) beamProcessors.add(bp);
-			
-			Integer minFB = null, maxFB = null;
-			for(Integer bp: beamProcessors) {
-				
-				Integer min = Integer.parseInt(mopsrBpCornerturnMap.get("BEAM_FIRST_RECV_" + bp));
-				Integer max = Integer.parseInt(mopsrBpCornerturnMap.get("BEAM_LAST_RECV_" + bp));
-				
-				
-				if(minFB == null || minFB > min)  minFB = min;
-				if(maxFB == null || maxFB < max)  maxFB = max;
-				
-			}
-			beamBoundariesMap.put(node, new Pair<Integer, Integer>(minFB + 1 , maxFB + 1));
-			
+
 		}
 		
-
+				
+	}
+	
+	public static String getBeamSubDir(String utc, Integer fanbeam){
+		String root = "";
 		
+		for(Entry<String, Map <Integer, Map < Integer , Pair<Integer, Integer>>>> e: nodeBsBpFbMap.entrySet()){
+			
+			for(Entry<Integer, Map < Integer , Pair<Integer, Integer>>> e2: e.getValue().entrySet()){
+				
+				if(e2.getKey().equals(edgeBS)) continue;
+				
+				for(Entry<Integer , Pair<Integer, Integer>> e3: e2.getValue().entrySet()){
+					
+					if(fanbeam >= e3.getValue().getValue0() && fanbeam <= e3.getValue().getValue1() ){
+						
+						 
+						root =	ConfigManager.getSmirfMap().get("BEAM_PROCESSOR_PREFIX")
+									 + String.format("%02d", e3.getKey()) + "/"
+									 + utc + "/"
+									 + ConfigManager.getSmirfMap().get("FB_DIR") + "/"
+									 + ConfigManager.getSmirfMap().get("BEAM_DIR_PREFIX") 
+									 + String.format("%03d", fanbeam);
+						return root;
+					}
+				}
+				
+			}
+		}
+		
+		
+		return root;
 	}
 
 	private static Map<String, String> readConfig(String file) throws IOException{
@@ -142,6 +206,12 @@ public class ConfigManager {
 				if(line.contains("#")) line = line.substring(0, line.indexOf("#"));
 				
 				String[] chunks = line.trim().split("\\s+",2);
+				
+				if(chunks.length != 2){
+					System.err.println("Bad line in " + file +" =>'" + line+"' . Ignoring line");
+					continue;
+				}
+				
 				map.put(chunks[0], chunks[1]);
 			}
 			br.close();
@@ -156,16 +226,40 @@ public class ConfigManager {
 
 	}
 	
-	
-	public static String getServerForFB(int fb){
-		for(Entry<String, Pair<Integer, Integer>> entry : beamBoundariesMap.entrySet()) if( (fb >= entry.getValue().getValue0()) && ( fb <= entry.getValue().getValue1() ) ) return entry.getKey();
-		return null;
-		
+	public static Integer getBeamSearcherForFB(int fb) throws InvalidFanBeamNumberException{
+
+		for(Entry<String,Map<Integer, Pair<Integer, Integer>>> entry : beamBoundariesMap.entrySet()){
+			
+			if(entry.getKey().equals(edgeNode)) continue;
+
+			for(Entry <Integer, Pair<Integer, Integer>> entry2: entry.getValue().entrySet()) {
+				
+				if(entry2.getKey().equals(edgeBS)) continue;
+
+
+				if( (fb >= entry2.getValue().getValue0()) && ( fb <= entry2.getValue().getValue1() ) ) return entry2.getKey();
+
+			}
+
+
+		}
+		throw new InvalidFanBeamNumberException(fb + "");
+
 	}
 	
-	public static Integer getServerNumberForFB(int fb){
-		return  getServerNumberForServerName(getServerForFB(fb));
-	}
+	
+//	public static String getServerForFB(int fb){
+//		for(Entry<String, Pair<Integer, Integer>> entry : beamBoundariesMap.entrySet()) if( (fb >= entry.getValue().getValue0()) && ( fb <= entry.getValue().getValue1() ) ) return entry.getKey();
+//		return null;
+//		
+//	}
+//	
+	
+	
+//	public static Integer getServerNumberForFB(int fb){
+//		return  getServerNumberForServerName(getServerForFB(fb));
+//	}
+//	
 	
 	public static  Integer getServerNumberForServerName(String name){
 		return (name == null) ? null : Integer.parseInt(name.replaceAll("\\D+", ""));
@@ -191,9 +285,7 @@ public class ConfigManager {
 		return mopsrBpCornerturnMap;
 	}
 
-	public static Map<String, Pair<Integer, Integer>> getBeamBoundariesMap() {
-		return beamBoundariesMap;
-	}
+
 
 	public static Integer getNumFanBeams() {
 		return numFanBeams;
@@ -220,6 +312,14 @@ public class ConfigManager {
 	public static Map<String, String> getSmirfConventionsMap() {
 		return smirfConventionsMap;
 	}
-	
 
+	public static Map<String, Map<Integer, Pair<Integer, Integer>>> getBeamBoundariesMap() {
+		return beamBoundariesMap;
+	}
+
+	public static Integer getEdgeBS() {
+		return edgeBS;
+	}
+
+	
 }
