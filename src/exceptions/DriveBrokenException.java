30a1542674d22cc82ee144529e94062ae4e5c5aa
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2017-05-15 04:37:03 +1000

    pretty print xml on error

--- src/exceptions/DriveBrokenException.java
+++ src/exceptions/DriveBrokenException.java
@@ -1,6 +1,18 @@
 package exceptions;
 
+import java.io.StringWriter;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import bean.TCCStatus;
+import util.Utilities;
 
 public class DriveBrokenException extends TCCException {
 
@@ -12,5 +24,8 @@ public class DriveBrokenException extends TCCException {
 	}
 	public DriveBrokenException(String message, String trace,TCCStatus status) {
 		super(message, trace,status);
+		
+		Utilities.prettyPrintXML(status.getXml());
+		
 	}
 }
