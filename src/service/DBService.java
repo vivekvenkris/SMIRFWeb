66c3e1b0e6cd244bcb5e1c311cfd88d31d100a2b
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2017-05-15 04:36:39 +1000

    Extra main

--- src/service/DBService.java
+++ src/service/DBService.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -11,6 +12,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 
+import bean.Angle;
 import bean.FluxCalibrator;
 import bean.Observation;
 import bean.ObservationSessionTO;
@@ -18,12 +20,15 @@ import bean.ObservationTO;
 import bean.ObservingSession;
 import bean.PhaseCalibrator;
 import bean.Pointing;
+import bean.TBSourceTO;
+import manager.DBManager;
+import manager.PSRCATManager;
 import util.SMIRFConstants;
 import util.Utilities;
 
 public class DBService implements SMIRFConstants {
 	private static EntityManagerFactory emFactory = Persistence.createEntityManagerFactory("SMIRFWeb");
-	
+
 	public static void addPointingsToDB(List<Pointing> gridPoints){
 		EntityManager entityManager = emFactory.createEntityManager( );
 		entityManager.getTransaction( ).begin( );
@@ -33,8 +38,8 @@ public class DBService implements SMIRFConstants {
 		entityManager.getTransaction().commit();
 		entityManager.close();
 	}
-	
-	
+
+
 	public static Pointing getPointingByID(Integer pointingID){
 		EntityManager entityManager = emFactory.createEntityManager( );
 		entityManager.getTransaction().begin();
@@ -43,30 +48,30 @@ public class DBService implements SMIRFConstants {
 		entityManager.close();
 		return pointing;
 	}
-	
-	
+
+
 	public static Pointing getPointingByUniqueName(String pointingName){
 		EntityManager entityManager = emFactory.createEntityManager( );
 		TypedQuery<Pointing> query = entityManager.createQuery("FROM Pointing t where t.pointingName =?1",Pointing.class);
 		Pointing result = query.setParameter(1, pointingName).getSingleResult();
 		return result;
 	}
-	
-	
+
+
 	public static FluxCalibrator getFluxCalByUniqueName(String name){
 		EntityManager entityManager = emFactory.createEntityManager( );
 		TypedQuery<FluxCalibrator> query = entityManager.createQuery("FROM FluxCalibrator t where t.sourceName =?1",FluxCalibrator.class);
 		FluxCalibrator result = query.setParameter(1, name).getSingleResult();
 		return result;
 	}
-	
+
 	public static PhaseCalibrator getPhaseCalByUniqueName(String name){
 		EntityManager entityManager = emFactory.createEntityManager( );
 		TypedQuery<PhaseCalibrator> query = entityManager.createQuery("FROM PhaseCalibrator t where t.sourceName =?1",PhaseCalibrator.class);
 		PhaseCalibrator result = query.setParameter(1, name).getSingleResult();
 		return result;
 	}
-	
+
 	public static List<Pointing> getAllPointings(){
 		EntityManager entityManager = emFactory.createEntityManager( );
 		entityManager.getTransaction().begin();
@@ -75,9 +80,9 @@ public class DBService implements SMIRFConstants {
 		entityManager.getTransaction().commit();
 		entityManager.close();
 		return pointings;
-	
+
 	}
-	
+
 	public static List<Pointing> getAllUnobservedPointingsOrderByPriority(){
 		EntityManager entityManager = emFactory.createEntityManager( );
 		entityManager.getTransaction().begin();
@@ -88,9 +93,9 @@ public class DBService implements SMIRFConstants {
 		entityManager.getTransaction().commit();
 		entityManager.close();
 		return pointings;
-	
+
 	}
-	
+
 	public static List<String> getAllPointingTypes(){
 		EntityManager entityManager = emFactory.createEntityManager( );
 		entityManager.getTransaction().begin();
@@ -100,17 +105,17 @@ public class DBService implements SMIRFConstants {
 		entityManager.close();
 		return uniqPointingTYpes;
 	}
-	
+
 	public static List<Pointing> getAllPointingsForPointingType(String pointingType){
 		EntityManager entityManager = emFactory.createEntityManager( );
 		TypedQuery<Pointing> query = entityManager.createQuery("FROM Pointing t where t.type =?1",Pointing.class);
 		List<Pointing> result = query.setParameter(1, pointingType).getResultList();
 		return result;
 	}
-	
-	
-	
-	
+
+
+
+
 	public static Pointing incrementPointingObservations(Integer pointingID){
 		EntityManager entityManager = emFactory.createEntityManager( );
 		entityManager.getTransaction().begin();
@@ -121,9 +126,9 @@ public class DBService implements SMIRFConstants {
 		return pointing;
 	}
 
-	
-	
-	
+
+
+
 	public static PhaseCalibrator getCalibratorByID(Integer calibratorID){
 		EntityManager entityManager = emFactory.createEntityManager( );
 		entityManager.getTransaction().begin();
@@ -132,24 +137,24 @@ public class DBService implements SMIRFConstants {
 		entityManager.close();
 		return calibrator;
 	}
-	
 
-	
+
+
 	public static PhaseCalibrator getPhaseCalibratorByName(String name){
 		EntityManager entityManager = emFactory.createEntityManager( );
 		TypedQuery<PhaseCalibrator> query = entityManager.createQuery("select t from PhaseCalibrator t where t.sourceName =?1",PhaseCalibrator.class);
 		PhaseCalibrator result = query.setParameter(1, name).getSingleResult();
 		return result;
 	}
-	
+
 	public static FluxCalibrator getFluxCalibratorByName(String name){
 		EntityManager entityManager = emFactory.createEntityManager( );
 		TypedQuery<FluxCalibrator> query = entityManager.createQuery("select t from FluxCalibrator t where t.sourceName =?1",FluxCalibrator.class);
 		FluxCalibrator result = query.setParameter(1, name).getSingleResult();
 		return result;
 	}
-	
-	
+
+
 	public static List<PhaseCalibrator> getAllPhaseCalibrators(){
 		EntityManager entityManager = emFactory.createEntityManager( );
 		entityManager.getTransaction().begin();
@@ -158,9 +163,9 @@ public class DBService implements SMIRFConstants {
 		entityManager.getTransaction().commit();
 		entityManager.close();
 		return phaseCalibrators;
-	
+
 	}
-	
+
 	public static List<PhaseCalibrator> getAllPhaseCalibratorsOrderByFluxDesc(){
 		EntityManager entityManager = emFactory.createEntityManager( );
 		entityManager.getTransaction().begin();
@@ -169,10 +174,10 @@ public class DBService implements SMIRFConstants {
 		entityManager.getTransaction().commit();
 		entityManager.close();
 		return phaseCalibrators;
-	
+
 	}
-	
-	
+
+
 	public static List<FluxCalibrator> getAllFluxCalibrators(){
 		EntityManager entityManager = emFactory.createEntityManager( );
 		entityManager.getTransaction().begin();
@@ -181,9 +186,9 @@ public class DBService implements SMIRFConstants {
 		entityManager.getTransaction().commit();
 		entityManager.close();
 		return fluxCalibrators;
-	
+
 	}
-	
+
 	public static List<FluxCalibrator> getAllFluxCalibratorsOrderByDMDesc(){
 		EntityManager entityManager = emFactory.createEntityManager( );
 		entityManager.getTransaction().begin();
@@ -192,21 +197,41 @@ public class DBService implements SMIRFConstants {
 		entityManager.getTransaction().commit();
 		entityManager.close();
 		return fluxCalibrators;
-	
+
 	}
-	
-	
-	
+
+
+
 	public static void addObservationToDB(ObservationTO observationTO){
+
 		Observation observation = new Observation(observationTO);
 		EntityManager entityManager = emFactory.createEntityManager( );
 		entityManager.getTransaction().begin();
 		entityManager.persist(observation);
+		//entityManager.persist(observation.getObservingSession());
 		entityManager.getTransaction().commit();
 		entityManager.close();
 		observationTO.setObservationID(observation.getObservationID());
+
 	}
-	
+
+	public static void main(String[] args) {
+
+		ObservationTO to = new ObservationTO();
+		to.setUtc("2017-01-07-11:11:11.000");
+		to.setName("SMIRF_1610-5703");
+		to.setTobs(720);
+		to.setObservingSession(null);
+		to.setObsType(smcPointingSymbol);
+		to.setTiedBeamSources(Arrays.asList(new TBSourceTO[]{PSRCATManager.getTBSouceByName("J1141-6545"),PSRCATManager.getTBSouceByName("J1745-3040")}));
+		to.setObservingSession(DBService.getObservationSessionByID(701));
+		DBService.addObservationToDB(to);
+		
+	}
+
+
+
+
 	public static void addSessionToDB(ObservationSessionTO observationSessionTO){
 		ObservingSession observationSession = new ObservingSession(observationSessionTO);
 		EntityManager entityManager = emFactory.createEntityManager( );
@@ -216,6 +241,17 @@ public class DBService implements SMIRFConstants {
 		entityManager.close();
 		observationSessionTO.setSessionID(observationSession.getSessionID()); 
 	}
+	
+	public static ObservationSessionTO getObservationSessionByID(Integer observationSessionTO){
+		EntityManager entityManager = emFactory.createEntityManager( );
+		entityManager.getTransaction().begin();
+		ObservingSession observingSession = entityManager.find(ObservingSession.class, observationSessionTO);
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		return new ObservationSessionTO(observingSession);
+	}
+
+	
 
 	public static void makeObservationComplete(ObservationTO observationTO){
 		EntityManager entityManager = emFactory.createEntityManager( );
@@ -226,7 +262,7 @@ public class DBService implements SMIRFConstants {
 		entityManager.getTransaction().commit();
 		entityManager.close();
 	}
-	
+
 	public static void incrementCompletedObservation(ObservationSessionTO observationSessionTO){
 		EntityManager entityManager = emFactory.createEntityManager( );
 		entityManager.getTransaction().begin();
@@ -237,13 +273,10 @@ public class DBService implements SMIRFConstants {
 		entityManager.close();
 	}
 
-	
-	
 
-	public static void main(String[] args) {
-		
-		System.err.println(getAllUnobservedPointingsOrderByPriority());
-	}
+
+
+
 
 
 
