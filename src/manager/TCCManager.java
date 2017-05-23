5c40882cafebf3228366f42d35f697e8ae37a39f
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2017-05-24 04:18:06 +1000

    comments of print statemnets

--- src/manager/TCCManager.java
+++ src/manager/TCCManager.java
@@ -21,17 +21,17 @@ public class TCCManager {
 		Pair<Double,Double> tiltNSNow = tccStatus.getTiltNS();
 		
 		int rampTime = 10;
-		
-		System.err.println("source:"+srcRadNS*Constants.rad2Deg + " "+srcRadMD*Constants.rad2Deg);
-		System.err.println("status:" + " " + tiltNSNow.getValue0()*Constants.rad2Deg + " " + tiltNSNow.getValue1()*Constants.rad2Deg + " "
-		+ tiltMDNow.getValue0()*Constants.rad2Deg + " "+tiltMDNow.getValue1()*Constants.rad2Deg );
+		//
+		//System.err.println("source:"+srcRadNS*Constants.rad2Deg + " "+srcRadMD*Constants.rad2Deg);
+		//System.err.println("status:" + " " + tiltNSNow.getValue0()*Constants.rad2Deg + " " + tiltNSNow.getValue1()*Constants.rad2Deg + " "
+		//+ tiltMDNow.getValue0()*Constants.rad2Deg + " "+tiltMDNow.getValue1()*Constants.rad2Deg );
 		Pair<Double,Double> slewTimeNS = new Pair<Double, Double>(Math.abs((Double)tiltNSNow.getValue(0) - srcRadNS)*Constants.rad2Deg/TCCConstants.slewRateNSFast
 				, Math.abs((Double)tiltNSNow.getValue(1) - srcRadNS)*Constants.rad2Deg/TCCConstants.slewRateNSFast);
 		
 		Pair<Double,Double> slewTimeMD = new Pair<Double, Double>(Math.abs((Double)tiltMDNow.getValue(0) - srcRadMD)*Constants.rad2Deg/TCCConstants.slewRateMD
 				, Math.abs((Double)tiltMDNow.getValue(1) - srcRadMD)*Constants.rad2Deg/TCCConstants.slewRateMD);
 		
-		System.err.println("slew time:" + slewTimeNS.getValue0() + " "+slewTimeNS.getValue1()+ " "+slewTimeMD.getValue0()+ " "+slewTimeMD.getValue1());
+		//System.err.println("slew time:" + slewTimeNS.getValue0() + " "+slewTimeNS.getValue1()+ " "+slewTimeMD.getValue0()+ " "+slewTimeMD.getValue1());
 		
 		Double maxSlewTime = Collections.max(Arrays.asList(slewTimeNS.getValue0(),slewTimeNS.getValue1(),slewTimeMD.getValue0(),slewTimeMD.getValue1()));
 		return (int)(1.2*(maxSlewTime + 2*rampTime));
