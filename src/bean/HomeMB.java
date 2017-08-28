5aaaedd1f0ed1d88e3d7771e31732a8d27abad2b
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2017-08-28 21:01:48 +1000

    Now gets status from the status poller instead of directly asking TCC and backend.

--- src/bean/HomeMB.java
+++ src/bean/HomeMB.java
@@ -1,19 +1,11 @@
 package bean;
 
-import java.text.ParseException;
-
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 
-import exceptions.BackendException;
-import exceptions.TCCException;
-import manager.ScheduleManager;
-import service.BackendService;
-import service.TCCService;
-import service.TCCStatusService;
+import control.Control;
 
 @ManagedBean
 @ApplicationScoped
@@ -24,10 +16,6 @@ public class HomeMB {
 	private String TCCStatus;
 	private String SMIRFStatus;
 	private Integer timeElapsedPercent;
-	
-
-	private TCCStatusService tccService = new TCCStatusService();
-	private BackendService backendStatusService = BackendService.createBackendStatusInstance();
 
 	public HomeMB() {
 		backendStatus = TCCStatus = SMIRFStatus = null;
@@ -62,48 +50,30 @@ public class HomeMB {
 	public void setTimeElapsedPercent(Integer timeElapsedPercent) {
 		this.timeElapsedPercent = timeElapsedPercent;
 	}
-	
+
 	public void updateObs(){
-		this.observation = ScheduleManager.getCurrentObservation();
-		try {
-			
-			if(backendStatusService.isON()) this.backendStatus = backendStatusService.getBackendStatusForHomePage();
-			else 							this.backendStatus="OFF";
-			
-			TCCStatus tccStatus = tccService.getTelescopeStatus();
-			
-			if(tccStatus.isTelescopeIdle())          this.TCCStatus = "Idle";
-			
-			else if(tccService.isTelescopeDriving()) this.TCCStatus =  "driving";
-			
-			else if(tccStatus.isTelescopeTracking()) this.TCCStatus =  "tracking";
-			
-			else this.TCCStatus = "Idle";
-			
-		} catch (TCCException e) {
-			
-			this.TCCStatus =  "problem";
-			System.err.println("TCC Exception on home page: " + e.getMessage());
-			
-		}catch(BackendException e){
-			
-			this.backendStatus="problem";
-			System.err.println("Backend Exception on home page: " + e.getMessage());
-			
-		} catch (InterruptedException e) {
-			
-			this.backendStatus="problem";
-			System.err.println("Interrupted Exception on home page: " + e.getMessage());
-			
-		}
+		
+		if(!Control.isPolling()) return;
+		
+		System.err.println(Control.isPolling() + " " + backendStatus);
+		
+		this.observation = Control.getCurrentObservation();
+		BackendStatus backendStatus = Control.getBackendStatus();
+
+		this.backendStatus = backendStatus.toString();
+		
+		this.TCCStatus = Control.getTccStatus().getStatus();
+
+
+
 	}
-	
+
 	public void updateTimeElapsed(){
-			if(this.observation == null || this.observation.getUtc() == null || observation.getUtcDate() == null) {
-				this.timeElapsedPercent = 0;
-				return;
-			}
-			this.timeElapsedPercent = (int) ((new Date().getTime() - observation.getUtcDate().getTime())*100/(observation.getTobs()*1000));
+		if(this.observation == null || this.observation.getUtc() == null || observation.getUtcDate() == null) {
+			this.timeElapsedPercent = 0;
+			return;
+		}
+		this.timeElapsedPercent = (int) ((new Date().getTime() - observation.getUtcDate().getTime())*100/(observation.getTobs()*1000));
 	}
 
 }
