bf7dfa5ef7a4424b1cc43c2efa86de396c067c96
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2017-08-28 21:00:36 +1000

    Added precession code. Need to verify though. Works on glance.

--- src/service/EphemService.java
+++ src/service/EphemService.java
@@ -7,12 +7,20 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.JulianFields;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.jastronomy.jsofa.JSOFA;
+import org.jastronomy.jsofa.JSOFA.JulianDate;
+import org.jastronomy.jsofa.JSOFA.SphericalCoordinateEO;
+import org.jastronomy.jsofa.JSOFAIllegalParameter;
+import org.jastronomy.jsofa.JSOFAInternalError;
 
 import bean.Angle;
 import bean.CoordinateTO;
+import bean.PointingTO;
+import bean.TBSourceTO;
 import exceptions.CoordinateOverrideException;
 import exceptions.EmptyCoordinatesException;
+import exceptions.EphemException;
 import manager.MolongloCoordinateTransforms;
 import util.BackendConstants;
 import util.Constants;
@@ -22,12 +30,12 @@ public class EphemService {
 	public static  LocalDateTime getUTCTimestamp(){
 		return  LocalDateTime.now(Clock.systemUTC());
 	}
-	
+
 	public static String getUtcStringNow(){
 		Instant instant = Instant.now();
 		return instant.toString().replaceAll("T", "-").replaceAll("Z", "");
 	}
-	
+
 	public static  double getMJDNow(){
 		return getMJDAfterOffset(0);
 	}
@@ -37,21 +45,21 @@ public class EphemService {
 		return dateTime.getLong(JulianFields.MODIFIED_JULIAN_DAY) +(dateTime.getHour()*3600+dateTime.getMinute()*60+dateTime.getSecond())/86400.0;
 
 	}
-	
+
 	public static double getMJDForUTC(String utcStr){
 		LocalDateTime utc = LocalDateTime.parse(utcStr, DateTimeFormatter.ofPattern(BackendConstants.backendUTCFormatOfPattern));
 		double mjd = utc.getLong(JulianFields.MODIFIED_JULIAN_DAY) +(utc.getHour()*3600+utc.getMinute()*60+utc.getSecond())/86400.0;
 		return mjd;
 	}
-	
+
 	public static double getMJDForUTC(String utcStr, int offsetSecondsFromUTC){
 		LocalDateTime utc = LocalDateTime.parse(utcStr, DateTimeFormatter.ofPattern(BackendConstants.backendUTCFormatOfPattern));
 		utc = utc.plusSeconds(offsetSecondsFromUTC);
 		double mjd = utc.getLong(JulianFields.MODIFIED_JULIAN_DAY) +(utc.getHour()*3600+utc.getMinute()*60+utc.getSecond())/86400.0;
 		return mjd;
 	}
-	
-	
+
+
 	private static double getGAST (double mjd){	// convert MJD in UT1 to a LAST
 
 		final int num_leap_seconds = 26;
@@ -76,11 +84,11 @@ public class EphemService {
 		return getRadLMSTForMolonglo (getMJDNow());
 	}
 
-	
+
 	public static Angle getAngleLMSTForMolongloNow(){
 		return new Angle(getRadLMSTForMolongloNow(),Angle.HHMMSS);
 	}
-	
+
 	public static double getRadLMSTForMolonglo (double mjd)
 	{
 		double gast, last;
@@ -93,30 +101,87 @@ public class EphemService {
 		return ( w >= 0.0 ) ? w : w + two_pi;
 
 	}
-	
+
 	public static double getRadLMSTforMolonglo(String utcStr){
 		if(!utcStr.contains(".")) utcStr += ".000";
 		LocalDateTime utc = LocalDateTime.parse(utcStr, DateTimeFormatter.ofPattern(BackendConstants.backendUTCFormatOfPattern));
 		double mjd = utc.getLong(JulianFields.MODIFIED_JULIAN_DAY) +(utc.getHour()*3600+utc.getMinute()*60+utc.getSecond())/86400.0;
 		double last = EphemService.getRadLMSTForMolonglo(mjd);
 		return last;
-	}
-	
+	}		
+
 	public static double getRadLMSTforMolonglo(LocalDateTime utc){
 		double mjd = utc.getLong(JulianFields.MODIFIED_JULIAN_DAY) +(utc.getHour()*3600+utc.getMinute()*60+utc.getSecond())/86400.0;
 		double last = EphemService.getRadLMSTForMolonglo(mjd);
 		return last;
 	}
-	
+
 	public static Angle getHA(Angle lst, Angle ra){
 		return new Angle(lst.getRadianValue() - ra.getRadianValue(), Angle.HHMMSS);
 	}
-	
+
 	public static Angle getRA(Angle lst, Angle ha){
 		return new Angle(lst.getRadianValue() - ha.getRadianValue(),Angle.HHMMSS);
 	}
 	
 	
+	public static SphericalCoordinateEO precessToUTC( Angle ra, Angle dec, LocalDateTime utc) throws EphemException{
+		
+		double pr = 0;  // atan2 ( -354.45e-3 * DAS2R, cos(dc) );
+		double pd = 0;  // 595.35e-3 * DAS2R;
+
+		// Parallax (arcsec) and recession speed (km/s).
+		double px = 0;  // 164.99e-3;
+		double rv = 0;  // 0.0;
+		
+		try {
+			
+			JulianDate utcDate = JSOFA.jauDtf2d("UTC", utc.getYear(), utc.getMonthValue(), utc.getDayOfMonth(), 
+					utc.getHour(),utc.getMinute(), utc.getSecond() + utc.getNano()/1e9);
+			
+			JulianDate taiDate = JSOFA.jauUtctai(utcDate.djm0, utcDate.djm1);
+			JulianDate ttDate = JSOFA.jauTaitt(taiDate.djm0, taiDate.djm1);
+						
+			SphericalCoordinateEO coord = JSOFA.jauAtci13(ra.getRadianValue(), dec.getRadianValue(),
+					pr, pd, px, rv, ttDate.djm0, ttDate.djm1);
+			
+			return coord;
+		
+		} catch (JSOFAIllegalParameter | JSOFAInternalError e) {
+			throw new EphemException(e.getMessage(),ExceptionUtils.getStackTrace(e));
+		}
+
+		
+	}
+
+	public static PointingTO precessPointingToNow(PointingTO pointingTO) throws EphemException{
+		SphericalCoordinateEO coord =  precessToUTC(pointingTO.getAngleRA(), pointingTO.getAngleDEC(), getUTCTimestamp());
+		
+		pointingTO.setAngleRA( new Angle(coord.pos.alpha, Angle.HHMMSS));
+		pointingTO.setAngleDEC( new Angle(coord.pos.delta, Angle.HHMMSS));
+		
+		pointingTO.setPrecessed(true);
+		
+		return pointingTO;
+		
+	}
+	
+	public static TBSourceTO precessTBSourceToNow(TBSourceTO tbSourceTO) throws EphemException{
+		SphericalCoordinateEO coord =  precessToUTC(tbSourceTO.getAngleRA(), tbSourceTO.getAngleDEC(), getUTCTimestamp());
+		
+		TBSourceTO outTO = new TBSourceTO(tbSourceTO);
+		
+		outTO.setAngleRA( new Angle(coord.pos.alpha, Angle.HHMMSS));
+		outTO.setAngleDEC( new Angle(coord.pos.delta, Angle.DDMMSS));
+		
+		outTO.setPrecessed(true);
+		
+		System.err.println("Precessed " + tbSourceTO.getAngleRA() + " -> " + outTO.getAngleRA() + " and " +
+				tbSourceTO.getAngleDEC() +  " -> " + outTO.getAngleDEC());
+		
+		return outTO;
+		
+	}
 
 	public static void main(String[] args) throws EmptyCoordinatesException, CoordinateOverrideException {
 		LocalDateTime dateTime = LocalDateTime.now(Clock.systemUTC());
