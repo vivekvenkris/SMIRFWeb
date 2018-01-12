05f175432eae05486bfb663955014a0ea63015b4
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2018-01-13 07:25:43 +1100

    added frb fields to this scheduler.
    moved init function to base

--- src/manager/PulsarDynamicTransitScheduler.java
+++ src/manager/PulsarDynamicTransitScheduler.java
@@ -3,6 +3,8 @@ package manager;
 import java.util.List;
 
 import bean.PointingTO;
+import bean.UserInputs;
+import exceptions.SchedulerException;
 import util.SMIRFConstants;
 
 public class PulsarDynamicTransitScheduler extends DynamicTransitScheduler{
@@ -11,15 +13,32 @@ public class PulsarDynamicTransitScheduler extends DynamicTransitScheduler{
 	protected double futureLookUpTimeInHours() {
 		
 		/* negative because this is in the future */
-		return -11.59;
+		return -11.9;
 	}
 	
 	@Override
 	public List<PointingTO> getDefaultPointings() {
-		return DBManager.getAllPointingsForPointingType(SMIRFConstants.psrPointingSymbol);
+		List<PointingTO> list= DBManager.getAllPointingsForPointingType(SMIRFConstants.psrPointingSymbol);
+		list.addAll(DBManager.getAllPointingsForPointingType(SMIRFConstants.frbFieldPointingSymbol));
+		return list;
 	}
 	
 	
+	@Override
+	public void init(UserInputs userInputs) throws SchedulerException {
+		this.userInputs = userInputs;
+
+		this.userInputs.setPointingTOs(getDefaultPointings());
+		
+		System.err.println("Assed default pointings: "+ this.userInputs.getPointingTOs().size());
+		
+		this.userInputs.getPointingTOs().stream().forEach( f -> System.err.println(f.getPointingName()));
+		
+		if(!userInputs.getSchedulerType().equals(SMIRFConstants.pulsarDynamicTransitScheduler)){
+			throw new SchedulerException("Mismatch between scheduler object and type");
+		}
+
+	}
 
 	
 	
