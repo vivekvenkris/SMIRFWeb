cba28f476175a44aa4fa5859b27a3116c3873e85
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2018-01-23 18:48:15 +1100

    mail update

--- src/exceptions/SchedulerException.java
+++ src/exceptions/SchedulerException.java
@@ -1,17 +1,53 @@
 package exceptions;
 
+import java.io.File;
+import java.util.List;
+
+import mailer.InlineAttachment;
+import util.SMIRFConstants;
+import util.Utilities;
+
 public class SchedulerException extends CustomException {
 	
 	public SchedulerException(String message, String trace) {
 		super(message,trace);
+		this.level = SMIRFConstants.levelWarn;
 	}
 	
 	public SchedulerException(String message) {
 		super(message);
+		this.level = SMIRFConstants.levelWarn;
+
 	}
 	
 	protected SchedulerException(){
 		super();
+		this.level = SMIRFConstants.levelWarn;
+
+	}
+
+	@Override
+	public String getEmailSubject() {
+		return "Scheduler exception";
+	}
+
+	@Override
+	public String getEmailBody() {
+
+		
+		return Utilities.buildEmailBodyTextFromException(this);
+	}
+
+	@Override
+	public List<InlineAttachment> getEmailInline() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<File> getEmailAttachments() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	
