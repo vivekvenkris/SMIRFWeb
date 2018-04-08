04988bdf029b2fdd50c17ac9d050287ca7e90050
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2018-04-08 17:10:17 +1000

    Several changes to transitMD & xhtml:
    checks for other schedulers
    scheduler is now in another thread
    finish call is still buggy. please note.
    several unwanted UI removed from xhtml and corresponding variables
    removed. there is a transit_old.xhtml for reference

--- src/bean/TransitMB.java
+++ src/bean/TransitMB.java
@@ -1,16 +1,24 @@
 package bean;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 
 import control.Control;
 import exceptions.SchedulerException;
+import mailer.Mailer;
 import manager.DBManager;
 import manager.Schedulable;
 import manager.TransitScheduler;
@@ -19,15 +27,9 @@ import util.SMIRFConstants;
 import util.TCCConstants;
 
 @ManagedBean
-@ApplicationScoped
+@RequestScoped
 public class TransitMB extends BaseMB {
-	
-	String utc;
-	String enteredUTC;
-	Integer duration;
-	Integer tobs;
-	Integer durationUnits;
-	Integer tobsUnits;
+
 	Boolean phaseCal;
 	Boolean doPulsarSearch;
 	Boolean doTiming;
@@ -38,218 +40,223 @@ public class TransitMB extends BaseMB {
 	List<String> nsSpeeds;
 	PhaseCalibratorTO phaseCalibrator;
 	List<PhaseCalibratorTO> phaseCalibrators;
-	Boolean utcRendered = false;
 	Double nsOffsetDeg;
-	
+
 	Schedulable scheduler = null;
 
 
 	//TransitScheduleManager manager = new TransitScheduleManager();
 
-	
+
 	public TransitMB(){
-		utc = "now";
-		utcRendered = false;
 		phaseCal = false;
 		doPulsarSearch = true;
 		doTiming = true;
-		
+
 		enableBackend = true;
 		enableTCC = true;
-		
+
 		phaseCalibrators = DBManager.getAllPhaseCalibrators();
-		
-		tobs = SMIRFConstants.tobs;
-		duration = 10;
-		tobsUnits =1;
-		durationUnits = 3600;
+
 		nsOffsetDeg = 0.0;
-		
+
 		nsSpeeds = new ArrayList<>();
 		nsSpeeds.add("Fast");
 		nsSpeeds.add("Slow");
 		nsSpeed = nsSpeeds.get(0);
-		
+
+		observer = "VVK";
+
+
 	}
-	
-	
-	
+
+
+
 	public void startSMIRFTransit(ActionEvent event){
-		
+
 		if(Control.getScheduler() != null) {
 			addMessage("Stop observation before starting another.");
+			System.err.println("Stop observation before starting another.");
+			return;
 		}
 		
-		if(this.utc.equals("now")) utc = EphemService.getUtcStringNow();
-		else utc = enteredUTC + ".000";
+		try {
+			
+			if(Control.otherSchedulers()) {
+				addMessage("Stop other schedulers. Check email for more.");
+				System.err.println("Stop other schedulers. Check email for more.");
+				return;
+			}
+			
+			Control.writeControlFile();
+			
+			
+		} catch (IOException e1) {
+			addMessage("Could not sanity check obs.running");
+			System.err.println("Could not sanity check obs.running");
+			e1.printStackTrace();
+			return;
+		}
 
-		ObservationSessionTO observationSessionTO = new ObservationSessionTO(utc, observer, tobs * tobsUnits , duration * durationUnits, null, phaseCal, false, false, 0);
+		String utc = EphemService.getUtcStringNow();
+
+		ObservationSessionTO observationSessionTO = new ObservationSessionTO(utc, observer, -1 ,-1, null, phaseCal, false, false, 0);
 		DBManager.addSessionToDB(observationSessionTO);
-		
+
 		UserInputs inputs = new UserInputs();
-		
-		inputs.setSchedulerType(SMIRFConstants.dynamicTransitScheduler);
-		
+
+		inputs.setSchedulerType(SMIRFConstants.interfacedDynamicScheduler);
+
 		inputs.setUtcStart(utc);
-		inputs.setTobsInSecs(tobs * tobsUnits);
-		inputs.setSessionTimeInSecs( duration * durationUnits );
-		
+
 		inputs.setDoPulsarSearching(doPulsarSearch);
 		inputs.setDoPulsarTiming(doTiming);
-		inputs.setEnableTCC(true);
-		inputs.setEnableBackend(true);
+		inputs.setEnableTCC(enableTCC);
+		inputs.setEnableBackend(enableBackend);
 		inputs.setMdTransit(true);
 		inputs.setObserver(observer);
 		inputs.setNsOffsetInDeg(nsOffsetDeg);
 		inputs.setNsSpeed(nsSpeed.equals("Fast") ? TCCConstants.slewRateNSFast : TCCConstants.slewRateNSSlow );
+//		inputs.setPointingTOs(
+//				DBManager
+//					.getAllPointings()
+//						.stream()
+//						.filter(f -> !f.getPointingName().startsWith("SMIRF_"))
+//						.collect(Collectors.toList())); 
+//		System.err.println("Added SMIRF pointing lsit");
 		inputs.setPointingTOs(new ArrayList<>());
 		
-		try {
-		scheduler = TransitScheduler.createInstance(SMIRFConstants.dynamicTransitScheduler);
+		inputs.setTobsInSecs(SMIRFConstants.tobs);
+
+		Callable<Boolean> schedulerCallable = new Callable<Boolean>() {
+
+			@Override
+			public Boolean call() throws Exception {
+
+
+				try {
+					scheduler = TransitScheduler.createInstance(SMIRFConstants.interfacedDynamicScheduler);
+
+					scheduler.init(inputs);
+					scheduler.startAndOverlook();
+					
+					
+
+				}catch (SchedulerException e) {
+					System.err.println("Exception starting schedluer");
+					addMessage("Scheduler could not be started. Reason: " + e.getMessage(), ExceptionUtils.getStackTrace(e));
+					e.printStackTrace();
+					Mailer.sendEmail(e);
+
+				}
+
+				return true;
+			}
+		};
+
+		Future<Boolean> schedulerFuture =  Control.getExecutorService().submit(schedulerCallable);
 		
-		scheduler.init(inputs);
-		scheduler.start();
-		
-		}catch (SchedulerException e) {
-			System.err.println("Exception starting schedluer");
-			addMessage("Scheduler could not be started. Reason: " + e.getMessage(), ExceptionUtils.getStackTrace(e));
-		}
-		
-		
+		Mailer.sendEmail("SMIRF started at approximate UTC = " + EphemService.getUtcStringNow() + "  and Local = " + LocalDateTime.now() );
+				
 	}
-	
-	
-public void terminateSchedule(ActionEvent event){
-	
+
+
+	public void keepPageAlive(){
+				
+	}
+
+
+	public String terminateScheduleTest(){
+		terminateSchedule(null);
+		return "transit";
+	}
+
+	public String finishScheduleTest(){
+		finishSchedule(null);
+		return "transit";
+	}
+
+	public String startSMIRFTransitTest(){
+		startSMIRFTransit(null);
+		return "transit";
+	}
+
+
+	public void terminateSchedule(ActionEvent event){
+
 
 		if(Control.getScheduler() !=null){
-			
+
 			Control.getScheduler().terminate();
 			addMessage("Terminated.");
+			Mailer.sendEmail("SMIRF terminated at approximate UTC = " + EphemService.getUtcStringNow() + "  and Local = " + LocalDateTime.now() );
 			return;
 		}
-		
+
 		addMessage("No scheduler running to terminate.");
-//	
-//		if(Control.getCurrentObservation() ==null){
-//			addMessage("No observation running at the moment.");
-//			return;
-//		}
-//		
-//		Control.setTerminateCall(true);
-//		
-//		while(manager.getScheduler()!= null && manager.getScheduler().isDone());
-//		
-//		try {
-//			
-//			BackendService.createBackendInstance().stopBackend();
-//			TCCService.createTccInstance().stopTelescope();
-//
-//		} catch (BackendException e) {
-//			
-//			e.printStackTrace();
-//			addMessage(e.getMessage());
-//
-//			
-//		} catch (TCCException e) {
-//			
-//			e.printStackTrace();
-//			addMessage(e.getMessage());
-//
-//		}
-//		
-//		try {
-//			manager.cancelSmirfingObservation(Control.getCurrentObservation());
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//			addMessage(e.getMessage());
-//
-//		}
-//		Control.setTerminateCall(false);
-//
-//		addMessage("Terminated.");
+		//	
+		//		if(Control.getCurrentObservation() ==null){
+		//			addMessage("No observation running at the moment.");
+		//			return;
+		//		}
+		//		
+		//		Control.setTerminateCall(true);
+		//		
+		//		while(manager.getScheduler()!= null && manager.getScheduler().isDone());
+		//		
+		//		try {
+		//			
+		//			BackendService.createBackendInstance().stopBackend();
+		//			TCCService.createTccInstance().stopTelescope();
+		//
+		//		} catch (BackendException e) {
+		//			
+		//			e.printStackTrace();
+		//			addMessage(e.getMessage());
+		//
+		//			
+		//		} catch (TCCException e) {
+		//			
+		//			e.printStackTrace();
+		//			addMessage(e.getMessage());
+		//
+		//		}
+		//		
+		//		try {
+		//			manager.cancelSmirfingObservation(Control.getCurrentObservation());
+		//		} catch (IOException e) {
+		//			e.printStackTrace();
+		//			addMessage(e.getMessage());
+		//
+		//		}
+		//		Control.setTerminateCall(false);
+		//
+		//		addMessage("Terminated.");
 	}
 
 	public void finishSchedule(ActionEvent event){
-		
+
 
 		if(Control.getScheduler() !=null){
-			
+
 			Control.getScheduler().terminate();
 			addMessage("Schedule will finish at the end of this observation.");
 			return;
 		}
-		
+
 		addMessage("No scheduler running to finish.");
-		
-//		if(Control.getCurrentObservation() ==null){
-//			addMessage("No observation running at the moment.");
-//			return;
-//		}
-//		
-//		Control.setFinishCall(true);
-//		addMessage("Schedule will finish at the end of this observation.");
+
+		//		if(Control.getCurrentObservation() ==null){
+		//			addMessage("No observation running at the moment.");
+		//			return;
+		//		}
+		//		
+		//		Control.setFinishCall(true);
+		//		addMessage("Schedule will finish at the end of this observation.");
 
 	}
-	
-	public void toggleUTCInput(){
-		if(utc.equals("now")){
-			utcRendered = false;
-		}
-		else {
-			enteredUTC = EphemService.getUtcStringNow();
-			utcRendered = true;
 
-		}
-	}
 
-	public String getUtc() {
-		return utc;
-	}
-
-	public void setUtc(String utc) {
-		this.utc = utc;
-	}
-
-	public String getEnteredUTC() {
-		return enteredUTC;
-	}
-
-	public void setEnteredUTC(String enteredUTC) {
-		this.enteredUTC = enteredUTC;
-	}
-
-	public Integer getDuration() {
-		return duration;
-	}
-
-	public void setDuration(Integer duration) {
-		this.duration = duration;
-	}
-
-	public Integer getTobs() {
-		return tobs;
-	}
-
-	public void setTobs(Integer tobs) {
-		this.tobs = tobs;
-	}
-
-	public Integer getDurationUnits() {
-		return durationUnits;
-	}
-
-	public void setDurationUnits(Integer durationUnits) {
-		this.durationUnits = durationUnits;
-	}
-
-	public Integer getTobsUnits() {
-		return tobsUnits;
-	}
-
-	public void setTobsUnits(Integer tobsUnits) {
-		this.tobsUnits = tobsUnits;
-	}
 
 	public Boolean getPhaseCal() {
 		return phaseCal;
@@ -332,13 +339,6 @@ public void terminateSchedule(ActionEvent event){
 		this.phaseCalibrators = phaseCalibrators;
 	}
 
-	public Boolean getUtcRendered() {
-		return utcRendered;
-	}
-
-	public void setUtcRendered(Boolean utcRendered) {
-		this.utcRendered = utcRendered;
-	}
 
 
 
