88e0e127f395d89e90ddb4980fe2c03b2e199b46
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2018-04-08 16:57:55 +1000

    blah commit

--- src/standalones/ResendStitches.java
+++ src/standalones/ResendStitches.java
@@ -1,33 +1,104 @@
 package standalones;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+import com.google.common.io.Files;
 
 import bean.ObservationTO;
+import exceptions.CoordinateOverrideException;
+import exceptions.EmptyCoordinatesException;
+import exceptions.InvalidFanBeamNumberException;
+import exceptions.ObservationException;
+import manager.DBManager;
+import manager.ObservationManager;
 import util.BackendConstants;
 import util.ConfigManager;
 
 public class ResendStitches {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, EmptyCoordinatesException, CoordinateOverrideException, InvalidFanBeamNumberException, InterruptedException, ObservationException {
 		
 		//List<String> utcs = Arrays.asList( new String[]{ "2017-05-25-15:39:21", "2017-05-25-15:52:17", "2017-05-25-16:05:41" } );
 		//List<String> utcs = Arrays.asList( new String[]{ "2017-05-25-15:52:17", "2017-05-25-16:05:41", "2017-05-25-16:18:13" } );
 //		List<String> utcs = Arrays.asList( new String[]{  "2017-05-26-14:55:40", "2017-05-26-15:08:56" } );
 		//List<String> utcs = Arrays.asList( new String[]{ "2017-05-26-12:43:32" } );
 		//List<String> utcs = Arrays.asList( new String[]{  "2017-05-27-15:02:52", "2017-05-27-15:15:36","2017-05-27-15:28:32" } );
-		List<String> utcs = Arrays.asList( new String[]{ "2017-05-29-12:46:48", "2017-05-29-12:59:28" , "2017-05-29-13:12:12" } );
+	//	List<String> utcs = Arrays.asList( new String[]{ "2017-05-29-12:46:48", "2017-05-29-12:59:28" , "2017-05-29-13:12:12" } );
+		
+		File file = new File("/home/vivek/SMIRF/jars/utcs_to_rerun.txt");
+		List<String> utcs = java.nio.file.Files.readAllLines(file.toPath());
+		
+		
+		utcs = Arrays.asList( new String[]{ "2018-03-21-19:03:46" } );
+		
+		System.err.println(utcs);
+
+		BufferedWriter bw = new BufferedWriter(new FileWriter(new File(file.getAbsolutePath()+".done")));
+		
+		ObservationManager manager = new ObservationManager();
+		
+		int i=1;
+		
+		for(String utc: utcs) {
+			System.err.println("Waiting for previous SMIRF soups");
+			manager.waitForPreviousSMIRFSoups();
+			System.err.println("Adding a new one num:" + i + "utc:" + utc);
+			
+		
+			
+			ObservationTO observationTO = DBManager.getObservationByUTC(utc);
+			
+			
+			
+			
+			if(observationTO == null) {
+				continue;
+			}
+			
+			if(!observationTO.getCoords().getPointingTO().isSMIRFPointing()) {
+				
+				System.err.println("skipping non SMIRF pointing:" + observationTO.getCoords().getPointingTO().getPointingName() );
+				continue;
+			}
+
+			
+			System.err.println("Adding" + observationTO.getCoords().getPointingTO().getPointingName() + " " + observationTO.getUtc());
+			manager.sendUniqStitchesForObservation(observationTO);
+			System.err.println("sent unique stitches for the observation");
+			
+			bw.write(utc);
+						
+			Thread.sleep( 2 * 60 * 1000 );
+			i++;
+						
+		}
+		
+		bw.close();
+		
+		System.exit(0);
+
 		
 		for(String utc : utcs){
-			try {
+			try { 
+				
 				restartSmirfingObservation(utc.trim());
+				
+				break;
 			} catch (IOException e) { 
 				e.printStackTrace();
 			}
