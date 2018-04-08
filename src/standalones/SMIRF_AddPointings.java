770ef5f30352c8a4201e96f8ee23fed20046eef9
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2018-04-08 16:58:54 +1000

    skips comments and existing pointings

--- src/standalones/SMIRF_AddPointings.java
+++ src/standalones/SMIRF_AddPointings.java
@@ -5,22 +5,35 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.jastronomy.jsofa.JSOFA;
 import org.jastronomy.jsofa.JSOFA.SphericalCoordinate;
 
 import bean.Angle;
 import bean.Pointing;
+import bean.PointingTO;
+import manager.DBManager;
 import service.DBService;
+import util.SMIRFConstants;
 
 public class SMIRF_AddPointings {
 	
 	public static void main(String[] args) throws IOException {
 		// file of the form NAME RA DEC TYPE
-		List<String> list = Files.readAllLines(Paths.get("/home/vivek/frb.pointings"));
+		List<String> list = Files.readAllLines(Paths.get("/home/observer/schedule/FRBs_list.txt"));
+		
+		List<PointingTO> existing = DBManager.getAllPointings();
+		
 		List<Pointing> pointings = new ArrayList<>();
 		list.forEach( f -> {
 			System.err.println("Considering " + f);
+			
+			if(f.contains("#")) {
+				System.err.println("Ignoring comment: " + f);
+				return;
+			}
+			
 			String[] chunks = f.trim().split(" ");
 			
 			Pointing pointing = new Pointing();
@@ -39,6 +52,13 @@ public class SMIRF_AddPointings {
 
 			}
 			
+			if(existing.contains(new PointingTO(pointing))) {
+				
+				System.err.println("Skipping existing pointing: " + f);
+				return;
+				
+			}
+			
 			SphericalCoordinate sc = JSOFA.jauIcrs2g(pointing.getAngleRA().getRadianValue(),  pointing.getAngleDEC().getRadianValue());
 			pointing.setAngleLON(new Angle(sc.alpha, Angle.DDMMSS));
 			pointing.setAngleLAT(new Angle(sc.delta, Angle.DDMMSS));
@@ -46,13 +66,16 @@ public class SMIRF_AddPointings {
 			pointing.setNumObs(0);
 			pointing.setPriority(10);
 			pointing.setType(chunks[3]);
+			pointing.setTobs(SMIRFConstants.tobs);
+			pointing.setLeastCadanceInDays(null);
 		
 			pointings.add(pointing);
 			
 			
 		});
 		
-		DBService.addPointingsToDB(pointings);
+		System.err.println("will add the following pointings: " + pointings.stream().map(f -> f.getPointingName()).collect(Collectors.toList()) );
+		//DBService.addPointingsToDB(pointings);
 		
 		System.err.println("Done.");
 	}
